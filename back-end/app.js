625ed7ce2b6ace4f70b333c24ea4bf30558065c9
Biohazard06 <jeromeweberpro@gmail.com>
2019-10-11 18:06:08 +0200

    blank template mern

---
+++ back-end/app.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    if (req.method === 'OPTIONS') {
+        res.header("Access-Control-Allow-Methods", "PUT, POST, DELETE, GET");
+        return res.status(200).json({});
+    }
+    next();
+});
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
---
+++ front-end/src/components/App/App.js
@@ -0,0 +1,31 @@
+/*
+ * Main app component
+ */
+
+/* Module imports */
+import React, { Component } from 'react';
+
+/* Styles imports */
+import './App.css';
+
+
+/* App component */
+class App extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Hello world</p> 
+      </div>
+    );
+  }
+}
+
+export default App;
